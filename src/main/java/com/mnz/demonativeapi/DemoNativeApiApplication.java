2736f2c13e2a64f88ea8c980eae3ed14e97ee5af
Danilo Menezes <danilo.mnz@gmail.com>
2021-10-02 14:06:01 -0300

    initial commit, contain ping controller

---
+++ src/main/java/com/mnz/demonativeapi/DemoNativeApiApplication.java
@@ -0,0 +1,11 @@
+package com.mnz.demonativeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNativeApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoNativeApiApplication.class, args);
+    }
+}
